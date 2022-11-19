ccce2cc453bc336e7f412ce4ad24787c47a909fb
Jerathel Jean <jeratheljean@yahoo.com>
2022-11-18 21:14:42 -0500

    .....

--- server.js
+++ server.js
@@ -339,3 +339,72 @@ function plusMinus(arr) {
 }
 plusMinus([-4,3,-9,0,4,1])
 ///////////////////////////////////////////////////////////////////////////////////////////////
+//Staircase detail
+
+//This is a staircase of size n=4:
+
+//    #
+//   ##
+//  ###
+// ####
+//Its base and height are both equal to n . It is drawn using # symbols and spaces. 
+//The last line is not preceded by any spaces.
+
+//Write a program that prints a staircase of size n .
+
+//Function Description
+
+//Complete the staircase function in the editor below.
+
+//staircase has the following parameter(s):
+
+//int n: an integer
+//Print
+
+//Print a staircase as described above.
+
+//Input Format
+
+//A single integer, n, denoting the size of the staircase.
+
+//Constraints
+
+//0<n≤100
+
+//Output Format
+
+//Print a staircase of size n using # symbols and spaces.
+
+//Note: The last line must have 0 spaces in it.
+
+//Sample Input
+
+//6 
+//Sample Output
+
+//      #
+//     ##
+//    ###
+//   ####
+//  #####
+// ######
+// Explanation
+
+//The staircase is right-aligned, composed of # symbols and spaces, 
+//and has a height and width of n=6 .
+function staircase(n) {
+    // if(n <=0 || n > 100) {
+    //     return
+    // }
+    // Write your code here
+     for(let i=1; i<=n; i++){
+        for (let j=0; j<(n-i); j++) {
+            process.stdout.write(" ")
+        }
+        for (let j=0; j<i; j++) {
+            process.stdout.write("#")
+        }
+        process.stdout.write("\n")
+    }
+}
+console.log(staircase(3))
